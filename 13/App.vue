82ef47e2cacbde725344affdcab9aa84c05aaebc
lyj <1041528020@qq.com>
2023-10-15 17:46:10 +0800

    第二次提交

---
+++ 13/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+
+    <MyDialog>
+      <template v-slot:title>
+        开头
+      </template>
+    </MyDialog>
+
+
+    <MyDialog>
+      
+      <template #button>
+        按钮一
+      </template>
+    </MyDialog>
+  </div>
+</template>
+
+<script>
+import MyDialog from "./components/MyDialog.vue"
+export default {
+  data() {
+    return {}
+  },
+  components: {
+    MyDialog,
+  },
+}
+</script>
+
+<style>
+body {
+  background-color: #b3b3b3;
+}
+</style>
